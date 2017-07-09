66acc60aafb5b8cf34514275e78c6cc9b1fa51a7
Durward Craig Jr <Durward.CraigJr@outlook.com>
2017-07-09 16:06:11 -0500

    CoinPickup and FloatObject classes
    CoinPickup
    -Script is used to pick up coins
    -Uses a simple trigger to detect coins and initiate a float animation
    FloatAboveObj
    -Used to float an object above another object between a max and min

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,26 @@
+﻿/********************************************** //
+ * Created by: Durward Craig Jr               * //
+ * Contact Email: Durward.CraigJr@outlook.com * //
+ ********************************************** //
+ *////////////////////////////////////////////////
+
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        assignFloatScript(collision);
+        collision.enabled = false;
+    }
+
+    /// <summary>
+    /// Assigns the float script to the collided object.
+    /// </summary>
+    /// <param name="collision">The collision.</param>
+    private void assignFloatScript(Collider2D collision)
+    {
+        Destroy(collision.GetComponent<LinearMovement>());
+        collision.gameObject.AddComponent<FloatAboveObj>().FloatAboveObject(gameObject.transform);
+    }
+}
---
+++ Assets/Scripts/FloatAboveObj.cs
@@ -0,0 +1,112 @@
+﻿/********************************************** //
+ * Created by: Durward Craig Jr               * //
+ * Contact Email: Durward.CraigJr@outlook.com * //
+ ********************************************** //
+ *////////////////////////////////////////////////
+
+using UnityEngine;
+
+/// <summary>
+/// Used to float objects vertically above an object
+/// </summary>
+public class FloatAboveObj : MonoBehaviour {
+
+    /// <summary>
+    /// The height the object rises to
+    /// </summary>
+    public float maxHeight = 3f;
+
+    /// <summary>
+    /// The minimum height above the parent object the child object is allowed
+    /// </summary>
+    public float minHeight = 1f;
+
+    /// <summary>
+    /// The speed the object floats between the max and min height
+    /// </summary>
+    public float floatSpeed = 2;
+
+    /// <summary>
+    /// The object the child floats above
+    /// </summary>
+    private Transform parent;
+
+    /// <summary>
+    /// The direction the object is floating. True if up. False if down
+    /// </summary>
+    private bool direction;
+
+
+    /// <summary>
+    /// Floats a child object on top of a specified parent
+    /// </summary>
+    /// <param name="child">The child.</param>
+    /// <param name="parent">The parent.</param>
+    public void FloatAboveObject(Transform parent)
+    {
+        this.parent = parent;
+        positionObject();
+        direction = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (parent == null)
+            return;
+
+        getHeight();
+
+        floatObject();
+    }
+
+    /// <summary>
+    /// Positions the child object on the parent
+    /// </summary>
+    private void positionObject()
+    {
+        transform.position = parent.position + (Vector3.up * minHeight);
+    }
+
+    /// <summary>
+    /// Gets the height of the child object
+    /// </summary>
+    /// <returns></returns>
+    private float getHeight()
+    {
+        return Mathf.Abs(transform.position.y - parent.position.y);
+    }
+
+    /// <summary>
+    /// Floats the object above the parent
+    /// </summary>
+    private void floatObject()
+    {
+        if (direction)
+            direction = !raiseObject();
+
+        else
+            direction = lowerObject();
+    }
+
+    /// <summary>
+    /// Raises the object.
+    /// </summary>
+    /// <returns></returns>
+    private bool raiseObject()
+    {
+        transform.position += Vector3.up * floatSpeed * Time.deltaTime;
+        return transform.position.y >= maxHeight;
+    }
+
+    /// <summary>
+    /// Lowers the object.
+    /// </summary>
+    /// <returns></returns>
+    private bool lowerObject()
+    {
+        transform.position -= Vector3.up * floatSpeed * Time.deltaTime;
+        return transform.position.y <= parent.position.y + minHeight;
+    }
+
+
+}
